28644b9b14eec7d2edcb5acbbdad834ae222d264
julkar28 <julk20250@student.nurulfikri.ac.id>
2021-07-14 19:48:39 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+
+api = Api(app)
+
+CORS(app)
+
+class Jul(Resource):
+    def get(self):
+        response = {"msg": requests.get('https://carnine.000webhostapp.com/kontak')}
+        return response
+    
+
+api.add_resource(Jul, "/api", methods=["GET"])
+if __name__ == "__main__":
+    app.run(debug=True,port=5005)
