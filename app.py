14170bfb0587db0dbde91ca3d82391f0c375862f
Julkarnain <72872788+julkar28@users.noreply.github.com>
2021-08-06 08:59:36 +0700

    update app.py

--- app.py
+++ app.py
@@ -35,11 +35,11 @@ class Jul(Resource):
                 #requests.get(api_url+"/sendmessage?chat_id="+str(chatid)+"&text=Haloo, test webhooks dicoffeean.com.&parse_mode=HTML")
 
 class Coba(Resource):
-	def get(self):
-		ts = time.time()
-		for i in range(100):
-			res = r.get("https://shopee.co.id")
-		return time.time() - ts
+    def get(self):
+	ts = time.time()
+	for i in range(100):
+	    res = r.get("https://shopee.co.id")
+	return time.time() - ts
 			
     
 
